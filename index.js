279c73b785483319def47122abccaad6c8fc26dd
LauricioX <developerwebmasterpro@gmail.com>
2022-09-27 17:10:08 -0300

    arrays como parametro

--- index.js
+++ index.js
@@ -5,17 +5,30 @@ const app = express();
 app.set("view engine","ejs");
 
 app.get("/:nome/:idade",(req,res) =>{
+
+
   let nome = req.params.nome
   let idade = req.params.idade
   //como visto acima podemos resceber os parametros direto da url e passar para o nosso objeto
 
   //essas variaveis podem ser criadas diretamente fora ou dentro do objeto!
 
-  
+  let errorMessage = false
+
+  let produtos = [
+    {nome:"refrigerante",preco:3},
+    {nome:"escova de dentes",preco:6},
+    {nome:"creme pra cabelo",preco:13},
+    {nome:"salgadinho",preco:5},
+    {nome:"energetico",preco:15}
+  ]
+
 res.render("index",{
   nome:nome,
-  idade:idade
-});
+  idade:idade,
+  erroMsg:errorMessage,
+  produtos:produtos,
+ });
 });
 
 app.get("/sobre",(req,res) =>{

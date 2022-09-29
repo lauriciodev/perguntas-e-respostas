ef6bc2b722eeac3cd9e3e9cae00b32e186460e41
LauricioX <developerwebmasterpro@gmail.com>
2022-09-29 08:28:20 -0300

    pagina de pergunta especifica

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const perguntaModel = require("./databases/Pergunta");
 
 
 //databases
-
 connection
 .authenticate()
 .then(() =>{
@@ -24,8 +23,9 @@ app.use(bodyParse.urlencoded({extended:false}));
 app.set("view engine","ejs");
 app.use(express.static("public"))
 
-app.get("/",(req,res) =>{
 
+//pagina que vai listar as perguntas
+app.get("/",(req,res) =>{
 perguntaModel.findAll(
 {raw: true,
 order:[["id","DESC"]] //desc = decrescente || asc = crescente
@@ -34,16 +34,16 @@ order:[["id","DESC"]] //desc = decrescente || asc = crescente
     perguntas:perguntas
   });
 });
-
-
 });
 
 
+//pagina para fazer pergunta
 app.get("/perguntar", (req,res) =>{
   res.render("perguntar")
 })
 
 
+// salvando perguntas no banco
 app.post("/salvar",(req,res) =>{
   let titulo = req.body.titulo;
   let descricao = req.body.descricao;
@@ -54,8 +54,23 @@ app.post("/salvar",(req,res) =>{
   }).then(() =>{
     res.redirect("/");
   })
+})
 
-   
+
+// pagina de pergunta especifica 
+app.get("/pergunta/:id",(req,res)=>{
+  let id = req.params.id;
+   perguntaModel.findOne({
+    where: {id:id}
+   }).then(pergunta => {
+    if(pergunta != undefined){
+      res.render("pergunta",{
+        id:id
+      });
+    }else{
+      res.redirect("/")
+    }
+   })
 })
 
 app.listen(8080,(erro) => erro ? console.log("erro"):console.log("servidor ok"));
